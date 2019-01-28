61a21650ceaec628017a89bb0d15e40d80e88b0c
wisdomtooth <cbtsao47@gmail.com>
2019-01-28 16:57:11 -0600

    add fail and repair

--- __tests__/enhancer.spec.js
+++ __tests__/enhancer.spec.js
@@ -1,12 +1,33 @@
 const enhancer = require("../enhancer");
 const item = {
   originalName: "Lambda Sword",
-  name: `Lambda Sword`,
+  name: `[+15]Lambda Sword`,
   type: "Weapon",
   durability: 100,
   enhancementLvl: 15
 };
+const failItem = {
+  originalName: "Lambda Sword",
+  name: `Lambda Sword`,
+  type: "Weapon",
+  durability: 20,
+  enhancementLvl: 15
+};
 const expected = {
+  originalName: "Lambda Sword",
+  name: `[PRI] Lambda Sword`,
+  type: "Weapon",
+  durability: 100,
+  enhancementLvl: 16
+};
+const expectedFail = {
+  originalName: "Lambda Sword",
+  name: `[+15] Lambda Sword`,
+  type: "Weapon",
+  durability: 90,
+  enhancementLvl: 15
+};
+const repaired = {
   originalName: "Lambda Sword",
   name: `[PRI] Lambda Sword`,
   type: "Weapon",
@@ -18,7 +39,18 @@ describe("enchanting system", () => {
     test("adds 1 to enhancement", () => {
       expect(enhancer.success(item)).toEqual(expected);
     });
+    it("should throw an error if durability is not enough", () => {
+      expect(enhancer.success(failItem)).toThrow();
+    });
   });
-  //   describe("enchant fail");
-  //   describe("repair");
+  describe("enchant fail", () => {
+    test("decreases durability if fails", () => {
+      expect(enhancer.fail(item)).toEqual(expectedFail);
+    });
+  });
+  describe("repair", () => {
+    test("repair to full durability", () => {
+      expect(enhancer.repair(expected)).toEqual(repaired);
+    });
+  });
 });
--- enhancer.js
+++ enhancer.js
@@ -4,39 +4,69 @@ module.exports = {
   repair
 };
 
-function success(item) {
-  function enhance(item) {
-    item.enhancementLvl++;
-    switch (item.enhancementLvl) {
-      case 16:
-        item.name = `[PRI] ${item.originalName}`;
-        break;
-      case 17:
-        item.name = `[DUO] ${item.originalName}`;
-        break;
-      case 18:
-        item.name = `[TRI] ${item.originalName}`;
-        break;
-      case 19:
-        item.name = `[TET] ${item.originalName}`;
-        break;
-      case 20:
-        item.name = `[PEN] ${item.originalName}`;
-        break;
-      default:
-        item.name = `[+${item.enhancementLvl}] ${item.originalName}`;
-    }
+function enhance(item) {
+  item.enhancementLvl++;
+  switch (item.enhancementLvl) {
+    case 16:
+      item.name = `[PRI] ${item.originalName}`;
+      break;
+    case 17:
+      item.name = `[DUO] ${item.originalName}`;
+      break;
+    case 18:
+      item.name = `[TRI] ${item.originalName}`;
+      break;
+    case 19:
+      item.name = `[TET] ${item.originalName}`;
+      break;
+    case 20:
+      item.name = `[PEN] ${item.originalName}`;
+      break;
+    default:
+      item.name = `[+${item.enhancementLvl}] ${item.originalName}`;
   }
-  if (item.type === "Weapon") {
-    if (item.enhancementLvl <= 7) {
-      enhance(item);
-    } else if (item.enhancementLvl <= 20) {
-      enhance(item);
-    }
+}
+function success(item) {
+  if (item.enhancementLvl < 15 && item.durability < 25) {
+    throw new Error("repair your item");
+  } else if (item.enhancementLvl < 15 && item.durability >= 25) {
+    enhance(item);
+  } else if (item.enhancementLvl >= 15 && item.durability < 10) {
+    throw new Error("repair your item");
+  } else if (item.enhancementLvl >= 15 && item.durability >= 10) {
+    enhance(item);
   }
   return item;
 }
 
-function fail() {}
+function disenchant(item) {
+  switch (item.enhancementLvl) {
+    case 17:
+      item.name = `[PRI] ${item.originalName}`;
+      break;
+    case 18:
+      item.name = `[DUO] ${item.originalName}`;
+      break;
+    case 19:
+      item.name = `[TRI] ${item.originalName}`;
+      break;
+    default:
+      console.log("test");
+      item.name = `[+${item.enhancementLvl}] ${item.originalName}`;
+  }
+}
+function fail(item) {
+  if (item.enhancementLvl < 15) {
+    disenchant(item);
+    item.durability -= 5;
+  } else if (item.enhancementLvl < 20) {
+    disenchant(item);
+    item.durability -= 10;
+  }
+  return item;
+}
 
-function repair() {}
+function repair(item) {
+  item.durability = 100;
+  return item;
+}
